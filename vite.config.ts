9677c79ccd447f953cd82b2b4d24b9afc3f36c15
abdulrahman alahmed <Abdul.Alahmed2004@gmail.com>
2025-11-11 18:15:20 +0300

    Remove test block from vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-    coverage: {
-      reporter: ['text', 'lcov'],
-      exclude: ['src/tests']
-    }
-  }
 });
